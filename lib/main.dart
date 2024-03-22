fa251d00070cd98480fb9703c99135eae161717c
Mujtaba <mujtabakamil9@gmail.com>
2024-03-22 22:48:19 +0500

    errors resolving

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/cupertino.dart';
 import 'firebase_options.dart';
 // import 'package:loginpage/firebase_options.dart';
 
